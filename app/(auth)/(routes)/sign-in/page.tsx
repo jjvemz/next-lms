5cc5c9482d4dd0c6805e0d3b9c8ec673d24f6e4d
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-12-31 15:26:47 -0300

    Frontend architecture complete

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-700">Hello Pagina sin protección!</p>
+    </div>
+  );
+}
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <p>Pagina con protección</p>
+  )
+}
+
+export default Page;
